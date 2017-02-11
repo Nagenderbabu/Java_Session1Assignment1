c022daaf17ee600fe0c60c38a6f65aa165118884
Nagenderbabu <nagenderbabu34@gmail.com>
2017-02-11 15:56:38 +0530

    please reply as soon as possible

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=3;
+float b=4;
+int c;
+c=(int) (a+b);
+System.out.println(c);
+
+	}
+
+}
